d9ac6779b5c1d156f5a9373c041738a1c124684a
collinmesser <collin@collinmesser.com>
2017-07-21 07:48:15 -0400

    Init repository and initial commit

---
+++ spec/search-github-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SearchGithubView from '../lib/search-github-view';
+
+describe('SearchGithubView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
